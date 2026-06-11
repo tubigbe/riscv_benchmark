907b4b4b7e7dcc6aef9cb7faae89af4ef5019c4c
chenyoo <chenyoo.zhang@gmail.com>
2026-06-11 22:34:26 +0100

    Add descriptions of this code

--- serv_project/instr_counter.sh
+++ serv_project/instr_counter.sh
@@ -1,3 +1,9 @@
+# =================================================================
+# Static instruction counter for a given function in firmware.elf
+# Parses objdump disassembly, counts each unique mnemonic.
+# Usage: ./instr_counter.sh
+# Edit the awk pattern below to target a different function name.
+# =================================================================
 riscv64-unknown-elf-objdump -d /home/chenyoo/riscv_benchmark/serv_project/firmware.elf | awk '
 /<popcount>:/ { flag = 1; next }
 flag {
