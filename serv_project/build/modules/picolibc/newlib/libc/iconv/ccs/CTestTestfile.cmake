df5e13a3b010f32845818dd17a6920738eeb3a99
rory <zhenruishang@gmail.com>
2026-06-12 18:06:42 +0100

    New files enable us to generate waveforms

--- serv_project/build/modules/picolibc/newlib/libc/iconv/ccs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chenyoo/servWorkspace/modules/lib/picolibc/newlib/libc/iconv/ccs
-# Build directory: /home/chenyoo/servWorkspace/build/modules/picolibc/newlib/libc/iconv/ccs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("iconv_data")
--- serv_project/build/modules/picolibc/newlib/libm/fenv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chenyoo/servWorkspace/modules/lib/picolibc/newlib/libm/fenv
-# Build directory: /home/chenyoo/servWorkspace/build/modules/picolibc/newlib/libm/fenv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
