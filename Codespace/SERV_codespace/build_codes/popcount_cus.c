b0a8fd09c086f48b726b976a8e4def11381b610f
keziamarhce <keziamarchelinee@gmail.com>
2026-09-01 18:49:55 +0100

    test: popcount_sink UART print + gitignore v1 RTL copy
    - popcount_cus.c: popcnt_custom() writes popcount_sink then prints it via
      asm_uart_putchar (popcount_sink = 0x00000008 for input 19149)
    - startup.S: keep asm_uart_putchar bit-bang helper
    - .gitignore: ignore serv_project/serv_rtl_v1/ (v1 RTL snapshot)
    - drop stale POPCOUNT_VISION.md

--- Codespace/SERV_codespace/build_codes/popcount_cus.c
+++ Codespace/SERV_codespace/build_codes/popcount_cus.c
@@ -1,6 +1,21 @@
 #include <stdint.h>
 
+extern void asm_uart_putchar(unsigned int c);
+
 volatile uint32_t popcount_sink;
+
+static void putstr(const char *s) {
+    while (*s)
+        asm_uart_putchar((unsigned int)*s++);
+}
+
+static void puthex(uint32_t v) {
+    int i;
+    for (i = 7; i >= 0; i--) {
+        uint32_t nib = (v >> (i * 4)) & 0xF;
+        asm_uart_putchar(nib < 10 ? ('0' + nib) : ('A' + nib - 10));
+    }
+}
 
 volatile static unsigned int popcnt_custom(unsigned int val) {
     unsigned int rd;
@@ -10,9 +25,14 @@ volatile static unsigned int popcnt_custom(unsigned int val) {
 }
 
 int main(void){
-    uint32_t reg_x1 = 19149;
+    uint32_t reg_x1 = 0x0fffffff;
 
     popcount_sink = popcnt_custom(reg_x1);
 
+    putstr("popcount_sink = 0x");
+    puthex(popcount_sink);
+    putstr("\n");
+
+    *(volatile unsigned int *)0x90000000 = 0;
     return popcount_sink;
 }
